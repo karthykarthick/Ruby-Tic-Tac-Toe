76d2e0870651e63c6d56bdcac4d777d88ea31d7d
karthykarthick <karthylock@gmail.com>
2020-08-06 23:36:05 +0530

    added console ui

--- bin/main.rb
+++ bin/main.rb
@@ -2,12 +2,12 @@ puts "welcome to Tic Tac Toe"
 puts "Enter your Name (First Player): "
 player_one = gets.chomp
 #save the player_one name in this var
-
 puts "#{player_one} your symbol is X "
 
 puts "Enter your Name (Second Player): "
-second_player = gets.chomp
-puts "#{second_player} your symbol is O "
+player_two = gets.chomp
+#save the player_one name in this var
+puts "#{player_two} your symbol is O "
 
 
 puts "-------------"
@@ -17,3 +17,10 @@ puts "| g | h | i |"
 puts "-------------"
 
 
+puts "#{player_one} It is Your Turn"
+puts "#{player_two} It is Your Turn"
+
+puts "player_one or player_two is winner! or it is draw"
+puts "Do you want to play again (Y/N)?"
+replay = gets.chomp
+puts replay

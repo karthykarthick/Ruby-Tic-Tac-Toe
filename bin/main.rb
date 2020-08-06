cf62613489fc71b7b4cb454fe66eee7d550f1d1b
karthykarthick <karthylock@gmail.com>
2020-08-06 23:31:13 +0530

    added linters

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,19 @@
-puts "Tic Tac Toe"
+puts "welcome to Tic Tac Toe"
+puts "Enter your Name (First Player): "
+player_one = gets.chomp
+#save the player_one name in this var
+
+puts "#{player_one} your symbol is X "
+
+puts "Enter your Name (Second Player): "
+second_player = gets.chomp
+puts "#{second_player} your symbol is O "
+
+
+puts "-------------"
+puts "| a | b | c |"
+puts "| d | e | f |"
+puts "| g | h | i |"
+puts "-------------"
+
+

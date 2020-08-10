70191818993335559cddc9b82330e73b40d0c9b1
karthykarthick <karthylock@gmail.com>
2020-08-11 00:37:03 +0530

    removed linter errors

--- lib/players.rb
+++ lib/players.rb
@@ -1,9 +1,8 @@
 class Players
-    attr_reader :name
-    attr_reader :symbol
-    def initialize(name, symbol)
-      @name = name
-      @symbol = symbol
-    end
+  attr_reader :name
+  attr_reader :symbol
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
 end
-  
